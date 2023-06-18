5aa0986e94b7832114b6516a7781097b05752fc5
Gaurav Sablok <sablokg@gmail.com>
2023-06-18 08:14:33 +0530

    Update R_Data_Structure_Diff.R

--- R_Data_Structure_Diff.R
+++ R_Data_Structure_Diff.R
@@ -8,7 +8,6 @@ numeric(0)
 [1] 3
 [1] 6
 
-
 # if you want to store the results in a different subvector then 
 > my_sub_vector <- as.vector(length(my_vector))
 > for (i in seq_along(my_vector)-1){
@@ -23,4 +22,14 @@ subfunction <- function(x){
     for (i in seq_along(x)-1){
         print(x[i+1]-x[i])
     }
+}
+
+# if you want to apply this to a dataframe then i coded this 
+data <- read.csv("iris.csv", stringsAsFactors = FALSE) # reading the csv file
+colnames(data) # colnames
+unlist(data["sepal_length"],use.names = FALSE) # selecting the specified column and then unlisting the column dataset
+data_unlist <- unlist(data["sepal_length"],use.names = FALSE) # storing in a variable
+# applying the data structure
+for (i in seq_along(data_unlist)-1){ 
+     print(data_unlist[i+1]-data_unlist[i])
 }
